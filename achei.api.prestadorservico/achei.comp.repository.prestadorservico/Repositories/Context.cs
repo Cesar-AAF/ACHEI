2fce02fc669ceb19e6acfbac9233a75381c81d20
eduardobisneto <eduardo_vieira2@hotmail.com>
2020-08-11 23:21:07 -0300

    Melhorias na estrutura e na nomenclatura da api Separação do repositório genérico em um componente isolado (Precisamos descobrir como vamos disponibilizar o componente no Nugget)

---
+++ achei.api.prestadorservico/achei.comp.repository.prestadorservico/Repositories/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace achei.comp.repository.prestadorservico.Repositories
+{
+    public class Context : DbContext
+    {
+        IConfiguration Configuration { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PrestadorServico> PrestadorServico { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new PrestadorServicoMap(modelBuilder.Entity<PrestadorServico>());            
+        }
+    }
+}
